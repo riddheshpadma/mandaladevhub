63b78a9e020891a50f3b5b7e4987f40a1974aef7
riddheshpadma <riddheshpadma.dev@gmail.com>
2025-03-17 16:20:35 +0530

    first commit

--- src/app/api/contactus/route.ts
+++ src/app/api/contactus/route.ts
@@ -1,5 +1,5 @@
 import connect from "@/lib/db";
-import Contact from "../../../lib/modals/contactus";
+import Contact from "@/lib/modals/contactus";
 import { NextResponse } from "next/server";
 import { allowedOrigins } from "../ReqOrigin"; // Import allowed origins
 
@@ -7,24 +7,6 @@ const checkOrigin = (request: Request) => {
   const origin = request.headers.get("Origin") || request.headers.get("Referer");
   if (!origin || !allowedOrigins.includes(origin)) {
     return new NextResponse("Forbidden", { status: 403 });
-  }
-};
-
-export const GET = async (request: Request) => {
-  const originCheck = checkOrigin(request);
-  if (originCheck) return originCheck;
-
-  try {
-    await connect();
-    const contact = await Contact.find();
-    return new Response(JSON.stringify(contact), { status: 200 });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return new NextResponse("Error in fetching contact: " + error.message, {
-        status: 500,
-      });
-    }
-    return new NextResponse("Unknown error occurred", { status: 500 });
   }
 };
 
@@ -38,40 +20,16 @@ export const POST = async (request: Request) => {
     const contact = await new Contact(body);
     await contact.save();
 
+    console.log("Data saved:", contact); // Log the saved data
+
     return new NextResponse(
       JSON.stringify({ message: "Message sent", contact: contact.name }),
       { status: 201 }
     );
   } catch (error: unknown) {
     if (error instanceof Error) {
+      console.error("Error saving data:", error); // Log the error
       return new NextResponse("Error in sending message: " + error.message, {
-        status: 500,
-      });
-    }
-    return new NextResponse("Unknown error occurred", { status: 500 });
-  }
-};
-
-export const DELETE = async (request: Request) => {
-  const originCheck = checkOrigin(request);
-  if (originCheck) return originCheck;
-
-  try {
-    await connect();
-
-    // Extract query params from URL
-    const { searchParams } = new URL(request.url);
-    const contactId = searchParams.get("id");
-
-    if (!contactId) {
-      return new NextResponse("Contact ID is required", { status: 400 });
-    }
-
-    await Contact.findByIdAndDelete(contactId);
-    return new NextResponse("Message deleted successfully", { status: 200 });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return new NextResponse("Error in deleting message: " + error.message, {
         status: 500,
       });
     }
