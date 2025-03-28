551bf94a7331583643563774bfa9ac51d247ea18
riddheshpadma <riddheshpadma.dev@gmail.com>
2025-03-28 22:38:39 +0530

    Gudi Padwa Campaign Added

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       {/* Logo */}
       <div className="flex items-center">
         <Image
-          src="/assets/BrandLogoMD.png"
+          src="/assets/BrandGudiPadwa.png"
           alt="logo"
           width={100}
           height={100}
