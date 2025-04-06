e8e0bac3bf5cf760a974b4808d6e5c9277ac222e
riddheshpadma <riddheshpadma.dev@gmail.com>
2025-04-06 13:32:49 +0530

    Nav  Update

--- src/app/_components/Landing.tsx
+++ src/app/_components/Landing.tsx
@@ -26,10 +26,11 @@ const Landing = () => {
   };
   return (
     <div>
-      <div className="min-h-screen bg-pink-50 flex items-end justify-center">
       <div>
             <Navbar />
           </div>
+      <div className="min-h-screen bg-pink-50 flex items-end justify-center">
+      
         {/* Dialog Box - with higher z-index and animation */}
       {showDialog && (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-[1000] p-4 animate-fadeIn">
